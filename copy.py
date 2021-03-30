c69923b4d9ed12a551b4327c2116366d956070ba
SangwonKwak <kwaksw10101@naver.com>
2021-03-30 13:49:56 +0900

    glfw_version

--- copy.py
+++ copy.py
@@ -4,29 +4,6 @@ from OpenGL.GLU import *
 import numpy as np
 import copy 
 
-Left_pressed = False
-Right_pressed = False
-degree1 = 0.
-degree2 = 0.
-init_pos = np.array([0,0])
-eye = np.array([0.,0.,.1])
-at = np.array([0.,0.,0.])
-cameraUp = np.array([0.,1.,0.])
-scale = 1.
-trans = np.array([0.,0.,0.])
-t1 = 0.
-
-
-full_list = []
-START_FLAG = False
-ENABLE_FLAG = False
-tree = []
-motion_start = 0 
-sum_channel = 0
-cur_index = 0
-count = 0
-Frames = 0
-
 class Node:
     def __init__(self, name=""):
         self.name = name
@@ -55,7 +32,6 @@ class Parsing:
         self.frames = 0
     
     # Make tree structure for parsing hierarchical model
-    # make_tree(full_list, root, 0, channel_list)
     def make_tree(self, full_list, parent, line_num, channel_list):
         while True:
             line = full_list[line_num].lstrip().split(' ')
@@ -133,8 +109,11 @@ class Parsing:
         return postures
      
 class Draw:
-    def __init__(self, motion):
-        self.motion = motion
+    def __init__(self, event_handle):
+        self.event_handle = event_handle
+        self.varr = None
+        self.iarr = None
+        self.cur_index = 0
     
     def drawframe(self):
         glBegin(GL_LINES)
@@ -150,7 +129,7 @@ class Draw:
         glEnd()
 
     def createVertexAndIndexArrayIndexed(self):
-        varr = np.array([
+        self.varr = np.array([
                 (-0.5773502691896258, 0.5773502691896258, 0.5773502691896258),
                 ( -1 ,  1 ,  1 ), # v0
                 (0.8164965809277261, 0.4082482904638631, 0.4082482904638631),
@@ -168,7 +147,7 @@ class Draw:
                 (-0.8164965809277261, -0.4082482904638631, -0.4082482904638631),
                 ( -1 , -1 , -1 ), # v7
                 ], 'float32')
-        iarr = np.array([
+        self.iarr = np.array([
                 (0,2,1),
                 (0,3,2),
                 (4,5,6),
@@ -182,17 +161,13 @@ class Draw:
                 (0,7,3),
                 (0,4,7),
                 ])
-        return varr, iarr
-
+        
     def drawCube_glDrawElements(self):
-        global gVertexArrayIndexed, gIndexArray
-        varr = gVertexArrayIndexed
-        iarr = gIndexArray
         glEnableClientState(GL_VERTEX_ARRAY)
         glEnableClientState(GL_NORMAL_ARRAY)
-        glNormalPointer(GL_FLOAT, 6*varr.itemsize, varr)
-        glVertexPointer(3, GL_FLOAT, 6*varr.itemsize, ctypes.c_void_p(varr.ctypes.data + 3*varr.itemsize))
-        glDrawElements(GL_TRIANGLES, iarr.size, GL_UNSIGNED_INT,iarr)
+        glNormalPointer(GL_FLOAT, 6*self.varr.itemsize, self.varr)
+        glVertexPointer(3, GL_FLOAT, 6*self.varr.itemsize, ctypes.c_void_p(self.varr.ctypes.data + 3*self.varr.itemsize))
+        glDrawElements(GL_TRIANGLES, self.iarr.size, GL_UNSIGNED_INT,self.iarr)
 
     def get_RotationM(self, offset):
         a = 1.0
@@ -209,7 +184,6 @@ class Draw:
         return R
 
     def draw_proper_cube(self, offset):
-        global gVertexArrayIndexed
         M = np.identity(4)
         half = np.sqrt(np.dot(offset,offset)) / 2.
         M[:3,:3] = get_RotationM(offset)
@@ -223,221 +197,218 @@ class Draw:
         drawCube_glDrawElements()
         glPopMatrix()
 
-# motion_index = 0
-# def draw_Model(node,motion_data):
-#     global motion_index
-#     # End site
-#     glPushMatrix()
-#     M = np.identity(4)
-#     if node.channel_count == 0:
-#         ###cube draw
-#         draw_proper_cube(node.offset)
-#         #######
-#     # joint & root
-#     else:
-#         # root
-#         if node.tree_index == 0:
-#             glTranslatef(motion_data[0],motion_data[1],motion_data[2])
-#             motion_index = 3
-#         ######cube draw
-#         draw_proper_cube(node.offset)
-#         #######
-#         M[:-1,3] = [node.offset[0], node.offset[1], node.offset[2]]
+    # draw_Model(motion.skeleton.root, index), index의 0은 몇번째 posture 사용할지 1은 몇번째 Rmatrix 사용할지
+    def draw_Model(self, node, index):
+        glPushMatrix()
+        # End site
+        if node.name == "__END__":
+            draw_proper_cube(node.offset)
         
-#         for i in node.channel:
-#             R = np.identity(4)
-#             th = np.radians(motion_data[motion_index])
-#             if i == "XROT":
-#                 R[:3,:3] = [[1., 0., 0.],
-#                             [0., np.cos(th), -np.sin(th)],
-#                             [0., np.sin(th), np.cos(th)]
-#                             ]
-#             elif i == "YROT":
-#                 R[:3,:3] = [[np.cos(th), 0., np.sin(th)],
-#                             [0., 1., 0.],
-#                             [-np.sin(th), 0., np.cos(th)]
-#                             ]
-#             elif i == "ZROT":
-#                 R[:3,:3] = [[np.cos(th), -np.sin(th), 0.],
-#                             [np.sin(th), np.cos(th), 0.],
-#                             [0., 0., 1.]
-#                             ]
-#             M = M @ R
-#             motion_index += 1
-#         glMultMatrixf(M.T)
-    
-#         for child in node.child:
-#             draw_Model(child,motion_data)
-#     glPopMatrix()
-
-def render():
-    global full_list, START_FLAG, ENABLE_FLAG, motion_start, tree, sum_channel, cur_index, motion_index, Frames, count
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glEnable(GL_DEPTH_TEST)
-    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
-    
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-scale,scale,-scale,scale,-1,1)
-    
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    real_eye = eye + trans
-    real_at = at + trans
-    gluLookAt(real_eye[0],real_eye[1],real_eye[2],real_at[0],real_at[1],real_at[2],cameraUp[0],cameraUp[1],cameraUp[2])
-    
-    drawframe()
-    drawgrid()
-    
-    glPushMatrix()
-    
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
-    glEnable(GL_LIGHT1)
-    glEnable(GL_LIGHT2)
-    glEnable(GL_LIGHT3)
-    glEnable(GL_LIGHT4)
-    glEnable(GL_NORMALIZE)
-    
-    # Light setting
-    lightPos0 = (1., 1., 1., 1.)
-    lightPos1 = (1., 1., -1., 1.)
-    lightPos2 = (-1., 1., 1., 1.)
-    lightPos3 = (-1., 1., -1., 1.)
-    lightPos4 = (0., -1., 0., 0.)
-    glLightfv(GL_LIGHT0,GL_POSITION,lightPos0)
-    glLightfv(GL_LIGHT1,GL_POSITION,lightPos1)
-    glLightfv(GL_LIGHT2,GL_POSITION,lightPos2)
-    glLightfv(GL_LIGHT3,GL_POSITION,lightPos3)
-    glLightfv(GL_LIGHT4,GL_POSITION,lightPos4)
-    
-    lightColor1 = (.45, .45, .45, .45)
-    lightColor2 = (.3, .3, .3, .3)
-    ambientLightColor = (.1,.1,.1,1.)
-    glLightfv(GL_LIGHT0,GL_DIFFUSE,lightColor1)
-    glLightfv(GL_LIGHT0,GL_SPECULAR,lightColor1)
-    glLightfv(GL_LIGHT0,GL_AMBIENT,ambientLightColor)
-    glLightfv(GL_LIGHT1,GL_DIFFUSE,lightColor1)
-    glLightfv(GL_LIGHT1,GL_SPECULAR,lightColor1)
-    glLightfv(GL_LIGHT1,GL_AMBIENT,ambientLightColor)
-    glLightfv(GL_LIGHT2,GL_DIFFUSE,lightColor1)
-    glLightfv(GL_LIGHT2,GL_SPECULAR,lightColor1)
-    glLightfv(GL_LIGHT2,GL_AMBIENT,ambientLightColor)
-    glLightfv(GL_LIGHT3,GL_DIFFUSE,lightColor1)
-    glLightfv(GL_LIGHT3,GL_SPECULAR,lightColor1)
-    glLightfv(GL_LIGHT3,GL_AMBIENT,ambientLightColor)
-    glLightfv(GL_LIGHT4,GL_DIFFUSE,lightColor2)
-    glLightfv(GL_LIGHT4,GL_SPECULAR,lightColor2)
-    glLightfv(GL_LIGHT4,GL_AMBIENT,ambientLightColor)
-    
-    # Object color setting
-    objectColor = (.3, .3, .7, 1.)
-    specularObjectColor = (1.,1.,1.,1.)
-    glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,objectColor)
-    glMaterialfv(GL_FRONT,GL_SHININESS,100)
-    glMaterialfv(GL_FRONT,GL_SPECULAR,specularObjectColor)
-    motion_data = []
-    scale_ratio = 0.3
-
-    # Drawing
-    if ENABLE_FLAG: 
-        if START_FLAG:
-            if count == Frames:
-                cur_index = motion_start + 3
-                count = 0
-            motion_data = full_list[cur_index].split(' ')
-            if len(motion_data) == 1:
-                motion_data = full_list[cur_index].split('\t')
-            for i in range(sum_channel):
-                motion_data[i] = float(motion_data[i])
-            cur_index += 1
-            count += 1
+        # joint & root
         else:
-            motion_data = np.zeros(sum_channel)
-        glColor3ub(0,0,200)
-        glScalef(scale_ratio,scale_ratio,scale_ratio)
-        motion_index = 0
-        draw_Model(tree[0], motion_data)
+            draw_proper_cube(node.offset)
+            glTranslatef(node.offset[0], node.offset[1], node.offset[2])
+            M = self.event_handle.motion.postures[index[0]][index[1]]
+            glMultMatrixf(M.T)
+            index[1] += 1
+
+            for child in node.child:
+                draw_Model(child, index)
+        glPopMatrix()
     
-    glDisable(GL_LIGHTING)
-    glPopMatrix()
-
-def button_callback(window,button,action,mod):
-    global Left_pressed,Right_pressed,init_pos
-    if action == glfw.PRESS:
-        init_pos = glfw.get_cursor_pos(window)
+    def render(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
         
-        if button == glfw.MOUSE_BUTTON_LEFT:
-            Left_pressed = True
-        elif button == glfw.MOUSE_BUTTON_RIGHT:
-            Right_pressed = True
-    elif action == glfw.RELEASE:
-        if button == glfw.MOUSE_BUTTON_LEFT:
-            Left_pressed = False
-        elif button == glfw.MOUSE_BUTTON_RIGHT:
-            Right_pressed = False
-
-def cursor_callback(window,xpos,ypos):
-    global eye,at,degree1,degree2,trans,cameraUp
-    if Left_pressed:
-        degree1 += (init_pos[0] - xpos) * 0.02
-        degree2 += (-init_pos[1] + ypos) * 0.02
-        if degree2 >= 0.:
-            degree2 %= 360.
-        else:
-            degree2 %= -360.
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(-scale,scale,-scale,scale,-1,1)
         
-        if 90. <= degree2 and degree2 <= 270.:
-            cameraUp[1] = -1.
-        elif -90. >= degree2 and degree2 >= -270.:
-            cameraUp[1] = -1.
-        else:
-            cameraUp[1] = 1.
-        eye[0] = 0.1 * np.cos(np.radians(degree2)) * np.sin(np.radians(degree1))
-        eye[1] = 0.1 * np.sin(np.radians(degree2))
-        eye[2] = 0.1 * np.cos(np.radians(degree2)) * np.cos(np.radians(degree1))
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        real_eye = event_handle.eye + event_handle.trans
+        real_at = event_handle.at + event_handle.trans
+        gluLookAt(real_eye[0],real_eye[1],real_eye[2],real_at[0],real_at[1],real_at[2],cameraUp[0],cameraUp[1],cameraUp[2])
         
-    elif Right_pressed:
-        cameraFront = eye - at
-        temp1 = np.cross(cameraFront,cameraUp)
-        u = temp1/np.sqrt(np.dot(temp1,temp1))
-        temp2 = np.cross(u,cameraFront)
-        w = temp2/np.sqrt(np.dot(temp2,temp2))
-        trans += u *(-init_pos[0]+xpos)*0.0001
-        trans += w *(-init_pos[1]+ypos)*0.0001
+        drawframe()
+        drawgrid()
         
-def scroll_callback(window,xoffset,yoffset):
-    global scale
-    if scale <= 0.1 and yoffset == 1:
-        scale = 0.1
-        return
-    scale -= 0.1 * yoffset
+        glPushMatrix()
+        
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_LIGHT1)
+        glEnable(GL_LIGHT2)
+        glEnable(GL_LIGHT3)
+        glEnable(GL_LIGHT4)
+        glEnable(GL_NORMALIZE)
+        
+        # Light setting
+        lightPos0 = (1., 1., 1., 1.)
+        lightPos1 = (1., 1., -1., 1.)
+        lightPos2 = (-1., 1., 1., 1.)
+        lightPos3 = (-1., 1., -1., 1.)
+        lightPos4 = (0., -1., 0., 0.)
+        glLightfv(GL_LIGHT0,GL_POSITION,lightPos0)
+        glLightfv(GL_LIGHT1,GL_POSITION,lightPos1)
+        glLightfv(GL_LIGHT2,GL_POSITION,lightPos2)
+        glLightfv(GL_LIGHT3,GL_POSITION,lightPos3)
+        glLightfv(GL_LIGHT4,GL_POSITION,lightPos4)
+        
+        lightColor1 = (.45, .45, .45, .45)
+        lightColor2 = (.3, .3, .3, .3)
+        ambientLightColor = (.1,.1,.1,1.)
+        glLightfv(GL_LIGHT0,GL_DIFFUSE,lightColor1)
+        glLightfv(GL_LIGHT0,GL_SPECULAR,lightColor1)
+        glLightfv(GL_LIGHT0,GL_AMBIENT,ambientLightColor)
+        glLightfv(GL_LIGHT1,GL_DIFFUSE,lightColor1)
+        glLightfv(GL_LIGHT1,GL_SPECULAR,lightColor1)
+        glLightfv(GL_LIGHT1,GL_AMBIENT,ambientLightColor)
+        glLightfv(GL_LIGHT2,GL_DIFFUSE,lightColor1)
+        glLightfv(GL_LIGHT2,GL_SPECULAR,lightColor1)
+        glLightfv(GL_LIGHT2,GL_AMBIENT,ambientLightColor)
+        glLightfv(GL_LIGHT3,GL_DIFFUSE,lightColor1)
+        glLightfv(GL_LIGHT3,GL_SPECULAR,lightColor1)
+        glLightfv(GL_LIGHT3,GL_AMBIENT,ambientLightColor)
+        glLightfv(GL_LIGHT4,GL_DIFFUSE,lightColor2)
+        glLightfv(GL_LIGHT4,GL_SPECULAR,lightColor2)
+        glLightfv(GL_LIGHT4,GL_AMBIENT,ambientLightColor)
+        
+        # Object color setting
+        objectColor = (.3, .3, .7, 1.)
+        specularObjectColor = (1.,1.,1.,1.)
+        glMaterialfv(GL_FRONT,GL_AMBIENT_AND_DIFFUSE,objectColor)
+        glMaterialfv(GL_FRONT,GL_SHININESS,100)
+        glMaterialfv(GL_FRONT,GL_SPECULAR,specularObjectColor)
+        
+        # Model drawing
+        if self.event_handle.timeline_changed:
+            self.cur_index = self.event_handle.timeline
+            self.event_handle.timeline_changed = False
+        
+        scale_ratio = 0.3
+        if event_handle.ENABLE_FLAG:
+            myMotion = self.event_handle.motion
+            if cur_index == myMotion.frames:
+                event_handle.START_FLAG = False
+            index = [self.cur_index, 0]
+            if event_handle.START_FLAG:
+                self.cur_index += 1
+            glColor3ub(0,0,200)
+            glScalef(scale_ratio,scale_ratio,scale_ratio)
+            draw_Model(myMotion.skeleton.root, index)
+        
+        glDisable(GL_LIGHTING)
+        glPopMatrix()
 
-def drop_callback(window,paths):
-    global full_list, ENABLE_FLAG, START_FLAG
-    
-    ENABLE_FLAG = True
-    START_FLAG = False
-    file_name = ''.join(paths)
-    file = open(file_name,'r')
-    full_list = file.readlines()
-    make_tree()
-    print("File name: %s"%(file_name))
-    print_data()
-    print("###########################################################")
-    file.close()
 
-def key_callback(window, key, scancode, action, mods):
-    global START_FLAG
-    if action==glfw.PRESS or action == glfw.REPEAT:
-        if key == glfw.KEY_SPACE:
-            START_FLAG = True
+class Event_handle:
+    def __init__(self):
+        self.Left_pressed = False
+        self.Right_pressed = False
+        self.degree1 = 0.
+        self.degree2 = 0.
+        self.init_pos = np.array([0,0])
+        self.eye = np.array([0.,0.,.1])
+        self.at = np.array([0.,0.,0.])
+        self.cameraUp = np.array([0.,1.,0.])
+        self.scale = 1.
+        self.trans = np.array([0.,0.,0.])
+        self.full_list = []
+        self.START_FLAG = False
+        self.ENABLE_FLAG = False
+        self.timeline_changed = False
+        self.timeline = 0
+
+        self.motion = None
+
+    def button_callback(self,window,button,action,mod):
+        if action == glfw.PRESS:
+            self.init_pos = glfw.get_cursor_pos(window)
             
-gVertexArrayIndexed = None
-gIndexArray = None
+            if button == glfw.MOUSE_BUTTON_LEFT:
+                self.Left_pressed = True
+            elif button == glfw.MOUSE_BUTTON_RIGHT:
+                self.Right_pressed = True
+        elif action == glfw.RELEASE:
+            if button == glfw.MOUSE_BUTTON_LEFT:
+                self.Left_pressed = False
+            elif button == glfw.MOUSE_BUTTON_RIGHT:
+                self.Right_pressed = False
+
+    def cursor_callback(self,window,xpos,ypos):
+        if self.Left_pressed:
+            self.degree1 += (self.init_pos[0] - xpos) * 0.02
+            self.degree2 += (-self.init_pos[1] + ypos) * 0.02
+            if self.degree2 >= 0.:
+                self.degree2 %= 360.
+            else:
+                self.degree2 %= -360.
+            
+            if 90. <= self.degree2 and self.degree2 <= 270.:
+                self.cameraUp[1] = -1.
+            elif -90. >= self.degree2 and self.degree2 >= -270.:
+                self.cameraUp[1] = -1.
+            else:
+                self.cameraUp[1] = 1.
+            self.eye[0] = 0.1 * np.cos(np.radians(self.degree2)) * np.sin(np.radians(self.degree1))
+            self.eye[1] = 0.1 * np.sin(np.radians(self.degree2))
+            self.eye[2] = 0.1 * np.cos(np.radians(self.degree2)) * np.cos(np.radians(self.degree1))
+            
+        elif self.Right_pressed:
+            cameraFront = self.eye - self.at
+            temp1 = np.cross(cameraFront, self.cameraUp)
+            u = temp1/np.sqrt(np.dot(temp1,temp1))
+            temp2 = np.cross(u,cameraFront)
+            w = temp2/np.sqrt(np.dot(temp2,temp2))
+            self.trans += u *(-init_pos[0]+xpos)*0.0001
+            self.trans += w *(-init_pos[1]+ypos)*0.0001
+        
+    def scroll_callback(self,window,xoffset,yoffset): 
+        if self.scale <= 0.1 and yoffset == 1:
+            self.scale = 0.1
+            return
+        self.scale -= 0.1 * yoffset
+
+    def drop_callback(self,window,paths):
+        self.ENABLE_FLAG = True
+        self.START_FLAG = False
+        self.timeline_changed = True
+        self.timeline = 0
+        file_name = ''.join(paths)
+        file = open(file_name,'r')
+        
+        # Make "motion" object
+        parsing = Parsing()
+        self.full_list = file.readlines()
+        root = Node()
+        line_num = 0
+        channel_list = []
+        postures = []
+        line_num, channel_list = parsing.make_tree(self.full_list, root, line_num, channel_list)
+        postures = parsing.make_postures(self.full_list, line_num, channel_list)
+        skeleton = Skeleton(root, parsing.joint_num)
+        self.motion = Motion(skeleton, postures, parsing.frames)
+
+        print("File name: %s"%(file_name))
+        print_data()
+        print("###########################################################")
+        file.close()
+
+    def key_callback(self,window, key, scancode, action, mods):
+        if action==glfw.PRESS or action == glfw.REPEAT:
+            if key == glfw.KEY_SPACE:
+                self.START_FLAG = True
+            
+
+t1 = 0.
 def main():
-    
-    global t1, gVertexArrayIndexed, gIndexArray
+    global t1
+    event_handle = Event_handle()
+    draw = Draw(event_handle)
+    draw.createVertexAndIndexArrayIndexed()
+
     if not glfw.init():
         return
     t1 = glfw.get_time()
@@ -453,8 +424,6 @@ def main():
     glfw.set_drop_callback(window,drop_callback)
     glfw.set_key_callback(window,key_callback)
     glfw.swap_interval(1)
-    
-    gVertexArrayIndexed, gIndexArray = createVertexAndIndexArrayIndexed()
     
     while not glfw.window_should_close(window):
         glfw.poll_events()
